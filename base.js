4f780319540fae4d24c739b775dbdfaf5e21a1fd
Andrey Bogdanov <a.bogdanov@corp.mail.ru>
2013-05-06 15:08:52 +0400

    Separate collection code from base geometric

--- base.js
+++ base.js
@@ -1,44 +1,5 @@
 goog.provide('bay.geom.base')
 
-bay.geom.base.Create = function(){
-  return new bay.geom.base.Collection();
-}
-
-// *************************************** Collection **************************************** //
-bay.geom.base.Collection = function(){
-  this.list = [];
-}
-
-bay.geom.base.Collection.prototype.getElements = function(){
-  return this.list;
-}
-
-bay.geom.base.Collection.prototype.clear = function(){
-  for(var i=this.list.length - 1;i>=0;i--){
-    this.list[i].deleteElement();
-  }
-  this.list = [];
-}
-
-bay.geom.base.Collection.prototype.add = function(e){
-  this.list.push(e);
-  return this.list.length;
-}
-
-// list of elements near the given point
-bay.geom.base.Collection.prototype.getNeighbourList = function(x, y, d, onlyVisible){
-  var neighbourList = [];
-  for(var i=0;i<this.list.length;i++){
-    if (this.list[i].distance && (!onlyVisible || !this.list[i].hidden)) {
-      var dist = this.list[i].distance(x, y);
-      if (dist <= d){
-        neighbourList.push({element: this.list[i], distance: dist});
-      }
-    }
-  }
-  return neighbourList;
-}
-
 // *************************************** Element **************************************** //
 bay.geom.base.Element = function(){
   this.label = '';
